678ada812d621313289356c346e232a4a8c9db95
DaDeng <thunderhit@qq.com>
2021-12-11 11:16:04 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ import setuptools
 
 setup(
     name='pdfdocx',     # 包名字
-    version='0.1',   # 包版本
+    version='1.1',   # 包版本
     description='读取pdf、docx文件，返回文件内的文本数据。',   # 简单描述
     author='大邓',  # 作者
     author_email='thunderhit@qq.com',  # 邮箱
-    url='https://github.com/thunderhit/pdfdocx',      # 包的主页
+    url='https://github.com/hidadeng/pdfdocx',      # 包的主页
     packages=setuptools.find_packages(),
-    install_requires=['pdfminer.six==20200402', 'python-docx==0.8.10'],
+    install_requires=['PyMuPDF==1.19.2', 'python-docx==0.8.10'],
     python_requires='>=3.5',
     license="MIT",
     keywords=[
